8c332999b92ac80c8cef05b9601ecfb3d5e97bbe
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2020-11-20 17:12:56 +0100

    Fixing empty egg for pypi

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if os.path.isfile(requirementPath):
         
 setuptools.setup(
     name="keras_ctcmodel",
-    version="1.0.0",
+    version="1.0.0rc2",
     install_requires=install_requires,
     author="Cyprien Ruffino",
     author_email="ruffino.cyprien@gmail.com",
